1da09952801a447a9bee7598795637bc338b4e53
rahat <rsdrahat@gmail.com>
2021-08-06 22:58:01 +0100

    Updates

---
+++ precache-manifest.72dcdc541f60961ce0f5344b1651c2fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ae5b1e282bed57d5cd4d",
+    "url": "/static/js/main.c00d7f05.chunk.js"
+  },
+  {
+    "revision": "c7652bb8ab0a13c4364d",
+    "url": "/static/js/2.6cb2980a.chunk.js"
+  },
+  {
+    "revision": "ae5b1e282bed57d5cd4d",
+    "url": "/static/css/main.98650fe3.chunk.css"
+  },
+  {
+    "revision": "e64e391e073fa15d50378ce459dcb08b",
+    "url": "/index.html"
+  }
+];
